93e95a9da9b5f65d5a4822a1f273fa41bf6af27e
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-24 15:18:46 +0100

    add components file structure

---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieInformation = () => {
+  console.log(first);
+  return (
+    <div>
+      MovieInformation
+    </div>
+  );
+};
+
+export default MovieInformation;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+  console.log('lf');
+  return (
+    <div>
+      NavBar
+    </div>
+  );
+};
+
+export default NavBar;
