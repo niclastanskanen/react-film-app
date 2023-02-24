656d9a0af7639a95b65704957a6a277bb7ecfaea
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-24 16:49:57 +0100

    add Sidebar state in NavBar to not show Sidebar in mobile view

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
@@ -7,15 +7,18 @@ import { useTheme } from '@mui/material/styles';
 import useStyles from './styles';
 
 const NavBar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px)');
   const theme = useTheme();
   const isAuthenticated = true;
 
   return (
-    <AppBar position="fixed">
-      <Toolbar className={classes.toolbar}>
-        {isMobile && (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
           <IconButton
             color="inherit"
             edge="start"
@@ -25,43 +28,62 @@ const NavBar = () => {
           >
             <Menu />
           </IconButton>
-        )}
-        <IconButton
-          color="inherit"
-          sx={{ ml: 1 }}
-          onClick={() => {}}
-        >
-          {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
-        </IconButton>
-        {!isMobile && 'Search...'}
-        <div>
-          {!isAuthenticated ? (
-            <Button
-              color="inherit"
-              onClick={() => {}}
-            >
-              Login &nbsp; <AccountCircle />
-            </Button>
-          ) : (
-            <Button
-              color="inherit"
-              component={Link}
-              to="/profile/:id"
-              className={classes.linkButton}
-              onClick={() => {}}
-            >
-              {!isMobile && <>My Movies &nbsp;</>}
-              <Avatar
-                style={{ width: 30, height: 30 }}
-                alt="Profile"
-                src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
-              />
-            </Button>
           )}
-        </div>
-        {isMobile && 'Search...'}
-      </Toolbar>
-    </AppBar>
+          <IconButton
+            color="inherit"
+            sx={{ ml: 1 }}
+            onClick={() => {}}
+          >
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search...'}
+          <div>
+            {!isAuthenticated ? (
+              <Button
+                color="inherit"
+                onClick={() => {}}
+              >
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to="/profile/:id"
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search...'}
+        </Toolbar>
+      </AppBar>
+      <div>
+        <nav className={classes.drawer}>
+          {isMobile ? (
+            <Drawer
+              variant="temporary"
+              anchor="right"
+              open={mobileOpen}
+              className={classes.drawerBackground}
+              classes={{ paper: classes.drawerPaper }}
+              ModalProps={{ keepMounted: true }}
+            >
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          ) : (
+            <Drawer />
+          )}
+        </nav>
+      </div>
+    </>
   );
 };
 
