7531220b76978eb7ace2eaa88a8d290593a046ff
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-24 15:12:03 +0100

    add basic routes in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <div>
     <CssBaseline />
-    <h1>Hello World</h1>
+    <main>
+      <Routes>
+        <Route exact path="/movie/:id" element={<h1>Movie id</h1>} />
+        <Route exact path="/actors/:id" element={<h1>Actors</h1>} />
+        <Route exact path="/" element={<h1>Home</h1>} />
+        <Route exact path="/profile/:id" element={<h1>Profile</h1>} />
+      </Routes>
+    </main>
   </div>
 );
 
