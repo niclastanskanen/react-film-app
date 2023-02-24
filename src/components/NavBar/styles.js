f88169f5c7226ed51941dcd2c494f949b5e44198
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-24 18:19:31 +0100

    add style to Navbar Drawer for overlapping content

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,11 +1,13 @@
 import { makeStyles } from '@mui/styles';
 
+const drawerWidth = 240;
+
 export default makeStyles((theme) => ({
   toolbar: {
     height: '80px',
     display: 'flex',
     justifyContent: 'space-between',
-    marginLeft: '250px',
+    marginLeft: '240px',
     [theme.breakpoints.down('sm')]: {
       marginLeft: '0',
       flexWrap: 'wrap',
@@ -16,6 +18,15 @@ export default makeStyles((theme) => ({
     [theme.breakpoints.up('sm')]: {
       display: 'none',
     },
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  drawerPaper: {
+    width: drawerWidth,
   },
   linkButton: {
     '&:hover': {
