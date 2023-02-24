cd2e13bec869df878dc67d74b534a14c13e791c2
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-24 16:27:43 +0100

    add dark mode icons to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
 
 import useStyles from './styles';
 
 const NavBar = () => {
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px)');
+  const theme = useTheme();
 
   return (
     <AppBar position="fixed">
@@ -23,6 +25,13 @@ const NavBar = () => {
             <Menu />
           </IconButton>
         )}
+        <IconButton
+          color="inherit"
+          sx={{ ml: 1 }}
+          onClick={() => {}}
+        >
+          {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
