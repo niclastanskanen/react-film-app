ec6db90374fe04361974d50828ce43e72e871687
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-25 19:05:47 +0100

    add session token from API

--- src/utils/index.js
+++ src/utils/index.js
@@ -19,6 +19,24 @@ export const fetchToken = async () => {
       window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=${window.location.origin}/approved`;
     }
   } catch (error) {
+    console.log('Sorry, your token could not be created.');
+  }
+};
 
+export const getSessionId = async () => {
+  const token = localStorage.getItem('request_token');
+
+  if (token) {
+    try {
+      const { data: { session_id } } = await moviesApi.post('authentication/session/new', {
+        request_token: token,
+      });
+
+      localStorage.setItem('session_id', session_id);
+
+      return session_id;
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
