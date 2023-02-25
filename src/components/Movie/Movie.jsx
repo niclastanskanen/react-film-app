48fc25dfabb195477f2078c1449ebabe06cc5add
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-25 11:07:16 +0100

    add style to movie and import it to index.js and export in MovieList

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -7,10 +7,11 @@ import useStyles from './styles';
 const Movie = ({ movie, i }) => {
   const classes = useStyles();
   console.log(movie, i);
+
   return (
-    <div>
-      Movie
-    </div>
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
+      <Typography className={classes.title} variant="h5">{movie.title}</Typography>
+    </Grid>
   );
 };
 
