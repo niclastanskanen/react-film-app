1aeb57eed2a19c7f609cd56a697e47080a1b7431
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-26 19:45:44 +0100

    fix responsiveness adjustments

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import { useGetMoviesQuery } from '../../services/TMDB';
 import { MovieList, Pagination } from '..';
 
