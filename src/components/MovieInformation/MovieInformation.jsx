a10ac3ceb4d8c870782a203422a1138120fe0ae9
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-26 13:35:14 +0100

    add top cast with image from API to MovieInformation

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -83,6 +83,13 @@ const MovieInformation = () => {
         <Typography variant="h5" gutterBottom>
           Top Cast
         </Typography>
+        <Grid item container spacing={2}>
+          {data && data.credits?.cast?.map((character, i) => (
+            <Grid key={i} item xs={4} md={2} component={Link} to={`/actors/${character.id}`} style={{ textDecoration: 'none' }}>
+              <img className={classes.castImage} src={`https://image.tmdb.org/t/p/w500/${character.profile_path}`} alt={character.name} />
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
     </Grid>
   );
