7f34500b1b9c6d3b13088f731d88d0aec46076f7
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-26 19:04:18 +0100

    add Pagination with boilerplate

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography, Button } from '@mui/material'
+
+const useStyles from './styles'
+
+const Pagination = () => {
+    const styles = useStyles();
+  return (
+    <div>Pagination</div>
+  )
+}
+
+export default Pagination
