498ebd76fa8630ad8e3e3d88f7471cf27a355b2a
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-27 18:03:37 +0100

    fix featuredmovie dont show in movie list

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -11,7 +11,7 @@ const Movies = () => {
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
   const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
 
-  const numberOfMovies = lg ? 16 : 18;
+  const numberOfMovies = lg ? 17 : 19;
 
   if (isFetching) {
     return (
@@ -38,7 +38,7 @@ const Movies = () => {
   return (
     <div>
       <FeaturedMovie movie={data.results[0]} />
-      <MovieList movies={data} numberOfMovies={numberOfMovies} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages} />
     </div>
   );
