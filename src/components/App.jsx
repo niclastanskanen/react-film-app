a1328cb4899877de2f9132aa50f1b674085ff434
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-02-27 20:55:19 +0100

    fix approved path for productions in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
           <Route exact path="/movie/:id" element={<MovieInformation />} />
           <Route exact path="/actors/:id" element={<Actors />} />
           <Route exact path="/" element={<Movies />} />
-          <Route exact path="approved" element={<Movies />} />
+          <Route path="/approved" element={<Movies />} />
           <Route exact path="/profile/:id" element={<Profile />} />
         </Routes>
       </main>
